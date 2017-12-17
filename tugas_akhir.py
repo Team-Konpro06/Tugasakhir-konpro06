7422c6407add47cda9765cc291ba4677ed6afcbb
candasukmariski <sukmacanda@gmail.com>
2017-12-17 16:07:28 +0800

    menambahkan

--- tugas_akhir.py
+++ tugas_akhir.py
@@ -1,7 +1,8 @@
 def menu():
 	print("--------------------------------------------------------")
 	print("1. Perkalian")
-	print("2. Keluar Dari Program")
+	print("2. Upah Karyawan")
+	print("3. Keluar Dari Program")
 	print("--------------------------------------------------------\n")
 	pilih = int(input("Masukkan pilihan : "))
 	return pilih
@@ -15,19 +16,35 @@ def perkalian():
 		print(num,'x',i,'=',num*i)
 	print("--------------------------------------------------------\n")
 
+def upah_karyawan():
+	print("Menghitung upah kerja karyawan perminggu")
+	print("")
+	nama = str(input("Nama Anda: "))
+	jkerja = int(input("Masukan Jam Kerja: "))
+	upahperjam = int(input("Upah Per Jam: Rp."))
+	if(jkerja<=24):
+		upahtotal = (jkerja*upahperjam)*7
+	else:
+		upahtotal = jkerja*upahperjam+24*upahperjam
+	print("Upah kerja dalkam 1 minggu: Rp.",upahtotal)
+	print("--------------------------------------------------------")
+	print("Karyawan",nama,"menerima upah perminggu: Rp.",upahtotal)
+	print("--------------------------------------------------------\n")
+	
 print("\n")
 print("--------------------------------------------------------")           
 print("<<<<<<<<<<<< Selamat Datang Di Program Kami >>>>>>>>>>>>")
 print("--------------------------------------------------------")
 
 pilih = True
-while(pilih<2):
+while(pilih<3):
 	pilih = menu()
 	if(pilih==1):
 		perkalian()
+	elif(pilih==2):
+		upah_karyawan()
 	else:
 		print("")
 		print("--------------------------------------------------------")           
 		print("<<<<<< Trimakasih Telah Mengunjungi Program Kami >>>>>>>")
 		print("--------------------------------------------------------")
-
